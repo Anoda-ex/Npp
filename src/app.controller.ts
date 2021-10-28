77587eb7c37669278fb4aeec1171d67138d46e48
Anoda-ex <sasha15200201@gmail.com>
2021-10-28 11:58:59 +0300

    lab2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,22 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getNumbers():number {
+    return this.appService.getNumbers()
+  } 
+  getString():string {
+    return this.appService.getString()
+  } 
+  getArrays(): number[] {
+    return this.appService.getArrays()
+  } 
+  getEnums():  any {
+    return this.appService.getEnums()
+  }
+  getMultitypeArrays():(string|number)[] {
+    return this.appService.getMultitypeArrays()
+  } 
+  getMultitypeValues(): string|number {
+    return this.appService.getMultitypeValues()
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Type } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getNumbers():number {
+    return 23
+  } 
+  getString():string {
+    return 'Hello World!'
+  } 
+  getArrays(): number[] {
+    return [1,2,3]
+  } 
+  getEnums():  any {
+    return {l:1,s:3,a:4}
+  }
+  getMultitypeArrays():(string|number)[] {
+    return [1,3,"hello",4]
+  } 
+  getMultitypeValues(): string|number {
+    return "string|number"
+  }
+
 }
